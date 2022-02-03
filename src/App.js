d4d02f778860f5f7cff3d1689fa968d48d74cd46
nicoledodge <nicoledodge5@gmail.com>
2022-02-03 15:57:27 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+
+
+
 
 function App() {
+  const [ cookies, setCookieCount ] = useState('');
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <input
+            type="text" onInput={(e)=> setCookieCount(e.target.value)}/>
+
+        <button type="submit" onClick={(e)=> {
+
+          console.log(cookies);
+          console.log(`https://swapi.dev/api/people/?search=${cookies}`);
+
+          fetch(`https://swapi.dev/api/people/?search=${cookies}`)
+              .then(response => response.json())
+              .then(data => {
+                console.log(data);
+              })
+
+        }}>
+          Search
+        </button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
