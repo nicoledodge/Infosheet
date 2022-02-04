ca615dd8215939d0c62fc3cddf878ba5d692c2aa
nicoledodge <nicoledodge5@gmail.com>
2022-02-04 16:50:21 -0600

    finished with routes working

--- src/App.js
+++ src/App.js
@@ -1,70 +1,25 @@
 import './App.css';
-// import {useState} from "react";
-import {BrowserRouter as Router, Routes, Link, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Form from "./components/form";
 
-import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
 
 function App() {
-    // const [cookies, setCookieCount] = useState('');
-
-
     return (
         <div className="App">
             <Router>
                 <div className="App">
-                    <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                        <div className="container">
-                            <Link className="navbar-brand" to={"/sign-in"}>RemoteStack</Link>
-                            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                                <ul className="navbar-nav ml-auto">
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
-                                    </li>
-                                    <li className="nav-item">
-                                        <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </nav>
-
                     <div className="outer">
                         <div className="inner">
 
-                            {/*<Route exact path='/' element={<Login/>} />*/}
                             <Routes>
-                                <Route path="/" element={<SignUp/>} />
-                                {/*<Route path="/sign-up" element={<SignUp/>} />*/}
+                                <Route path="/" element={<Form/>}/>
                             </Routes>
-                            {/*<Route exact path='/' component={Login} />*/}
-                            {/*<Route path="/sign-in" component={Login} />*/}
-                            {/*<Route path="/sign-up" component={SignUp} />*/}
 
                         </div>
                     </div>
                 </div>
             </Router>
-            {/*<img src={logo} className="App-logo" alt="logo"/>*/}
-
-            {/*<input*/}
-            {/*    type="text" onInput={(e) => setCookieCount(e.target.value)}/>*/}
-
-            {/*<button type="submit" onClick={(e) => {*/}
-
-            {/*    console.log(cookies);*/}
-            {/*    console.log(`https://frontend-take-home.fetchrewards.com/form/occupations`);*/}
-
-            {/*    fetch(`https://frontend-take-home.fetchrewards.com/form/occupations`)*/}
-            {/*        .then(response => response.json())*/}
-            {/*        .then(data => {*/}
-            {/*            console.log(data);*/}
-            {/*        })*/}
-
-            {/*}}>*/}
-            {/*    Search*/}
-            {/*</button>*/}
         </div>
     );
 }
---
+++ src/components/form.js
@@ -0,0 +1,147 @@
+import React, {Component} from "react";
+import Select from "react-select";
+import axios from "axios";
+
+
+export default class Form extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectOptions: [],
+            selectStates: [],
+            fullName: "",
+            email: "",
+            password: "",
+            occupation: "",
+            state: "",
+        }
+    }
+
+    getOptions() {
+        axios.get('https://frontend-take-home.fetchrewards.com/form')
+            .then((res) => {
+
+                const occ = res.data?.occupations;
+
+                const options = occ && occ.map(d => ({
+                    "value": d,
+                    "label": d
+
+                }))
+
+                console.log(options)
+
+                this.setState({selectOptions: options})
+
+            })
+
+    }
+
+    getStateOptions() {
+        axios.get('https://frontend-take-home.fetchrewards.com/form')
+            .then((res) => {
+
+                const states = res.data?.states;
+
+                const options = states && states.map(d => ({
+                    "value": d.name,
+                    "label": d.name
+
+                }))
+
+                console.log(options);
+
+                this.setState({selectStates: options})
+
+            })
+
+    }
+
+    handleChange = (name, value) => {
+        this.setState({ [name]: value });
+        console.log(name, value)
+
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const data = {
+            name: this.state.fullName,
+            email: this.state.email,
+            password: this.state.password,
+            occupation: this.state.occupation,
+            state: this.state.state,
+        };
+        axios.post(`https://frontend-take-home.fetchrewards.com/form`, data )
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+                // return(res.data);
+            })
+        console.log(data)
+
+    }
+
+    componentDidMount() {
+        this.getOptions()
+        this.getStateOptions()
+    }
+
+    render() {
+
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <h3>Create User</h3>
+
+                <div className="form-group">
+                    <label>Full name</label>
+                    <input type="text"
+                           name="fullName"
+                           value={this.state.name}
+                           className="form-control"
+                           placeholder="Full name"
+                           onChange={(e) => this.handleChange(e.target.name, e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Email</label>
+                    <input type="email"
+                           name="email"
+                           className="form-control"
+                           placeholder="Enter email"
+                           value={this.state.name}
+                           onChange={(e) => this.handleChange(e.target.name, e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Password</label>
+                    <input type="password"
+                           name="password"
+                           className="form-control"
+                           placeholder="Enter password"
+                           value={this.state.name}
+                           onChange={(e) => this.handleChange(e.target.name, e.target.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Choose your Occupation</label>
+                    <Select options={this.state.selectOptions}
+                            value={this.state.name}
+                            onChange={(e) => this.handleChange("occupation", e.value)}/>
+                </div>
+
+                <div className="form-group">
+                    <label>Choose your State</label>
+                    <Select options={this.state.selectStates}
+                            value={this.state.name}
+                            onChange={(e) => this.handleChange("state", e.value)}/>
+                </div>
+
+                <button type="submit" onSubmit={this.handleSubmit} className="btn btn-dark btn-lg btn-block">Submit</button>
+
+            </form>
+        );
+    }
+}
